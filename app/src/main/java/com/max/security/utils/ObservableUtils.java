07d4ee39c8c845bdef3180fc71d03bc0b68379f9
neilxie <neil.xmc@gmail.com>
2016-09-02 09:55:14 +0800

    add project files

---
+++ app/src/main/java/com/max/security/utils/ObservableUtils.java
@@ -0,0 +1,17 @@
+package com.max.security.utils;
+
+import com.max.security.model.FileModel;
+
+import java.util.List;
+
+import rx.Observable;
+
+/**
+ * Created by Administrator on 2016/9/1.
+ */
+public class ObservableUtils {
+
+    public static Observable<List<FileModel>> getFileModelByType(int type) {
+        return null;
+    }
+}
