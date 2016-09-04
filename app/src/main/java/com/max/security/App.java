226f078fec769fe697f41f23511322753cce5661
neilxie <neil.xmc@gmail.com>
2016-09-04 21:46:53 +0800

    add db dao create

--- app/src/main/java/com/max/security/App.java
+++ app/src/main/java/com/max/security/App.java
@@ -1,20 +1,29 @@
 package com.max.security;
 
 import android.app.Application;
+import android.database.sqlite.SQLiteDatabase;
 
+import com.max.security.greendao.DaoMaster;
+import com.max.security.greendao.DaoSession;
+import com.max.security.greendao.FileModelsDao;
 import com.max.security.injector.component.AppComponent;
 import com.max.security.injector.component.DaggerAppComponent;
 import com.max.security.injector.module.AppModule;
+
+import de.greenrobot.dao.query.QueryBuilder;
 
 /**
  * Created by lgp on 2015/5/24.
  */
 public class App extends Application{
     private AppComponent mAppComponent;
+    private static DaoSession mDaoSession;
+
     @Override
     public void onCreate() {
         super.onCreate();
         initializeInjector();
+        setupDatabase();
     }
 
 
@@ -37,6 +46,19 @@ public class App extends Application{
 
     public AppComponent getAppComponent() {
         return mAppComponent;
+    }
+
+    private void setupDatabase() {
+        DaoMaster.DevOpenHelper devOpenHelper = new DaoMaster.DevOpenHelper(this, "SafeBox", null);
+        SQLiteDatabase db = devOpenHelper.getWritableDatabase();
+        DaoMaster daoMaster = new DaoMaster(db);
+        mDaoSession = daoMaster.newSession();
+        QueryBuilder.LOG_SQL = BuildConfig.DEBUG;
+        QueryBuilder.LOG_VALUES = BuildConfig.DEBUG;
+    }
+
+    public static FileModelsDao getFileModelDao() {
+        return mDaoSession.getFileModelsDao();
     }
 
 }
